043d85b76652ab73e9538b9d2066bb27d3344017
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-07-20 14:31:32 +0330

    add shadcn button and customized

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,8 +27,8 @@ const config = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: "#333333",
+          foreground: "#FFFFFF",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
