e9bb3db482278fe7fb1eae3e98b0fe374bc568c2
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-07-21 13:18:36 +0330

    move header left to seperate file

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import HeaderCenter from "./sections/header-center";
 import HeaderRight from "./sections/header-right";
+import HeaderLeft from "./sections/header-left";
 
 const Header: React.FC = () => {
   return (
     <div className="h-[76px] w-full flex justify-center bg-white">
       <div className="max-w-[1600px] w-full h-full flex items-center justify-between bg-transparent px-6">
-        <h2 className="w-[350px] text-lg text-black">Logo</h2>
+        <HeaderLeft />
         <HeaderCenter />
         <HeaderRight />
       </div>
