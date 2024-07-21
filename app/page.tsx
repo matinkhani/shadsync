b96de6d4fa2408566b65363d26ac4f7cf3fd10b1
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-07-21 12:00:45 +0330

    add container to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Container from "@/components/shared/container";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <Container grid className="">
+      <div className="h-[600px] bg-black col-span-full">Home Page</div>
+    </Container>
   );
 }
