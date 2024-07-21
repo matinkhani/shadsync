b2577b2e240670fd90a28809bd0762793d9466cb
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-07-21 11:58:33 +0330

    create footer

---
+++ components/layout/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="h-[351px] w-full flex justify-center bg-white">
+      <div className="max-w-[1340px] w-full h-full bg-transparent">Footer</div>
+    </div>
+  );
+};
+
+export default Footer;
