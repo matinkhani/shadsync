70af50be6105a5064df94c6575d8a521805d33c8
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-07-21 15:30:13 +0330

    add footer top and footer bottom in footer component

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import FooterBottom from "./sections/footer/footer-bottom";
+import FooterTop from "./sections/footer/footer-top";
 
 const Footer = () => {
   return (
     <div className="h-[351px] w-full flex justify-center bg-white text-black">
-      <div className="max-w-[1340px] w-full h-full bg-transparent">Footer</div>
+      <div className="max-w-[1340px] w-full h-full flex flex-col bg-transparent">
+        <FooterTop />
+        <FooterBottom />
+      </div>
     </div>
   );
 };
