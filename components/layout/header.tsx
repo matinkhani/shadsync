394b98a16b6c18b2514c747eb4fbcf367e979e2c
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-07-21 11:59:28 +0330

    create header

---
+++ components/layout/header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HeaderCenter from "./sections/header-center";
+
+const Header: React.FC = () => {
+  return (
+    <div className="h-[76px] w-full flex justify-center bg-white">
+      <div className="max-w-[1600px] w-full h-full flex items-center justify-between bg-transparent px-6">
+        <h2 className="w-[350px] text-lg">Logo</h2>
+        <HeaderCenter />
+        <div className="w-[350px] h-[46px] bg-red-600"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
