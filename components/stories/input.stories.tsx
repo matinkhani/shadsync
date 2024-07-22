26e3b2368ccc20698aa8d303a4fc6997b24065cb
Matin Khani <134707825+matinkhani@users.noreply.github.com>
2024-07-22 10:59:56 +0330

    create input storybook

---
+++ components/stories/input.stories.tsx
@@ -0,0 +1,63 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Input } from "../ui/input";
+
+const meta: Meta<typeof Input> = {
+  title: "Input",
+  component: Input,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: {
+    placeholder: "",
+  },
+  argTypes: {
+    placeholder: {
+      description: "Placeholder",
+      type: "string",
+    },
+    type: {
+      control: "select",
+      options: [
+        "button",
+        "checkbox",
+        "color",
+        "date",
+        "datetime-local",
+        "email",
+        "file",
+        "hidden",
+        "image",
+        "month",
+        "number",
+        "password",
+        "radio",
+        "range",
+        "reset",
+        "search",
+        "submit",
+        "tel",
+        "text",
+        "time",
+        "url",
+        "week",
+      ],
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Defualt: Story = {
+  args: {
+    placeholder: "Defualt Input",
+  },
+};
+
+export const PasswordInput: Story = {
+  args: {
+    placeholder: "Password Input",
+    type: "password",
+  },
+};
